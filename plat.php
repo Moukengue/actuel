c9c9e4d51279237e2f7270999f5172fb2494e1b4
moukengue aline <aline.moukengue80@gmail.com>
2023-05-26 12:21:08 +0200

    code

---
+++ plat.php
@@ -0,0 +1,43 @@
+<?php
+include('../header.php');
+include "DAO.php";
+$plats = affichePlat();
+
+// $cat = afficherplats();
+
+?>
+
+
+  <body>
+  <?php include_once('../menu.php'); ?>
+  
+    <!---Les plats-->
+   
+    <div class="row">
+    <?php foreach ($plats as $plat):?>
+      <div class="col">
+      <a href="plat_detail.php?plat_id=<?php echo $plat->id; ?>">
+        <div class="card">
+          <img
+          src= "images_the_district/fooda_actuel/<?=$plat -> image?>"class="img-responsive mw-100"
+            class="salade_cat-top"
+            alt="..."
+          />
+          <div class="card-body" style="width: 500px">
+            <h4 class="card-title"><?=$plat -> libelle?></h4>
+            <h5 class=""><?=$plat -> description?></h5>
+            <h6><?=$plat -> prix?>€</h6>
+            
+            <button type="submit" class="btn btn-outline-primary">Commande</button>
+          </div>
+        </div>
+      </a>
+    </div>
+      <?php endforeach; ?>
+  </div>
+
+    <!--fin plats-->
+    <?php 
+include_once('../footer.php') ?>
+    
+
--- public/db.php
+++ public/db.php
@@ -3,6 +3,10 @@ function ConnexionBase() {
 
     try 
     {
+        //connexion à la bdd phpmyadmin sur le serveur
+        // $connexion = new PDO('mysql:host=localhost;charset=utf8;dbname=aline', 'aline', 'mpiervine');
+
+        //connexion à la bdd phpmyadmin en local 
         $connexion = new PDO('mysql:host=localhost;charset=utf8;dbname=the_district', 'admin', 'mpiervine20');
         $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $connexion;
